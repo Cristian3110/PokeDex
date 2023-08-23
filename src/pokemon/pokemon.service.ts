4761fa1eb733440e6031e0f561d4f26f56e34c47
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-08-23 18:47:16 -0400

    configuration of the method delete, ready

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -79,8 +79,10 @@ export class PokemonService {
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();
+    // this.pokemonModel.findByIdAndDelete(id);
   }
 
   private handleExecptions(error: any) {
