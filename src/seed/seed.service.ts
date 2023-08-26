32e94480eaa5a7ca3d98b1480ea1995841ebb7df
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-08-25 23:14:07 -0400

    configuration of the add poke in the DB from SEED

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
@@ -11,17 +19,21 @@ export class SeedService {
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       // console.log({ name, url });
 
       const segments = url.split('/');
 
       // adding (+) to convert to number
       const no = +segments[segments.length - 2];
-      console.log({ name, no });
+
+      //creating data poke
+      const pokemon = await this.pokemonModel.create({ name, no });
+
+      // console.log({ pokemon });
     });
 
     // console.log(data);
-    return data.results;
+    return `Seed execute`;
   }
 }
